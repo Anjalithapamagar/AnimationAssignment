842c3285d8d0411fe5923b1d94d6b772d8f66161
Anjali <anjalimagar666@gmail.com>
2024-03-21 14:47:07 -0400

    Implement Screen 1 Animated Content

--- app/src/main/java/com/mapd721/animationassignment/MainActivity.kt
+++ app/src/main/java/com/mapd721/animationassignment/MainActivity.kt
@@ -8,15 +8,24 @@ import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
@@ -34,6 +43,7 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "main") {
                     composable("main") { MainScreen(navController) }
+                    composable("screen1") { Screen1() }
                 }
             }
         }
@@ -62,6 +72,87 @@ fun MainScreen(navController: NavController) {
         Button(onClick = { navController.navigate("screen4") }) {
             Text(text = "Animation 4 Demo")
         }
+        Spacer(modifier = Modifier.height(200.dp))
+
+        Card(
+            modifier = Modifier
+                .width(300.dp)
+                .heightIn(min = 56.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text(text = "Anjali Thapa Magar", fontWeight = FontWeight.Bold)
+                Text(text = "301372527", fontWeight = FontWeight.Bold)
+            }
+        }
+    }
+}
+
+@Composable
+fun Screen1() {
+    AnimatedContent()
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun AnimatedContent(){
+    var count by remember { mutableStateOf(0) }
+    var screenBackgroundColor by remember { mutableStateOf(Color.White) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(screenBackgroundColor),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.AddCircle,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(32.dp)
+                    .clickable {
+                        count++
+                        screenBackgroundColor= if (count % 2 == 0) Color.Blue else Color.Gray
+                    }
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        ) {
+            AnimatedContent(
+                targetState = count,
+                transitionSpec = {
+                    if (targetState > initialState) {
+                        slideInVertically { height -> height } + fadeIn() with
+                                slideOutVertically { height -> -height } + fadeOut()
+                    } else {
+                        slideInVertically { height -> -height } + fadeIn() with
+                                slideOutVertically { height -> height } + fadeOut()
+                    }.using(
+                        SizeTransform(clip = false)
+                    )
+                }, label = ""
+            ) { targetCount ->
+                Text(
+                    text = "Count: $targetCount",
+                    modifier = Modifier.padding(16.dp),
+                    color = Color.White
+                )
+            }
+        }
     }
 }
 
@@ -71,6 +162,7 @@ fun MyApp() {
 
     NavHost(navController = navController, startDestination = "main") {
         composable("main") { MainScreen(navController) }
+        composable("screen1") { Screen1() }
     }
 }
 
